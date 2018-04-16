8f64bdd7ee4bf785f79743f7490e26db11a5d69f
Devang Patel <ddevang93@gmail.com>
2018-04-15 21:04:25 -0700

    livy https request add

--- src/airflow_livy_operator.py
+++ src/airflow_livy_operator.py
@@ -13,22 +13,34 @@ class SparkLivyOperator(BaseOperator):
             user,
             password,
             session_kind,
-            https_conn,
+            https_con,
             poll_interval=30,
             op_kwargs=None,
             *args, **kwargs):
         super(SparkLivyOperator, self).__init__(*args, **kwargs)
         self.user = user,
         self.password = password,
-        self.https_conn = https_conn,
         self.session_kind = session_kind
         print(op_kwargs)
+        self.livy_url = https_con
         self.payload = json.dumps(op_kwargs)
 
     def execute(self, context):
         print("Spark Livy Operator initiated.....")
         print("payload created is ....")
         print(self.payload)
+        headers = {
+            "content-type": "application/json",
+            "cache-control": "no-cache"
+        }
+        try:
+            response = requests.request("POST", url= self.https_con, auth=(self.user, self.password), data=self.payload, headers=headers)
+            print(response.text)
+            response_params = json.loads(response.text)
+            print(response_params)
+        except:
+            print("Unable to submit spark livy job.")
+            raise
 
 
 class SparkLivyPlugin(AirflowPlugin):
