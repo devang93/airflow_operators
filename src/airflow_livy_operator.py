c30be43d2cb49e68ffb8ed873569c9d29dbf17d3
Devang Anilkumar Patel <depatel@depatel012657.starbucks.net>
2018-04-16 20:56:17 -0700

    livy with polling

--- src/airflow_livy_operator.py
+++ src/airflow_livy_operator.py
@@ -1,8 +1,9 @@
 from airflow.plugins_manager import AirflowPlugin
 from airflow.models import BaseOperator
-import time, json, requests
-# from airflow.utils import apply_defaults
+from time import sleep
 from airflow.utils.decorators import apply_defaults
+import json, requests
+from airflow.exceptions import AirflowException
 
 
 class SparkLivyOperator(BaseOperator):
@@ -18,30 +19,59 @@ class SparkLivyOperator(BaseOperator):
             op_kwargs=None,
             *args, **kwargs):
         super(SparkLivyOperator, self).__init__(*args, **kwargs)
-        self.user = user,
-        self.password = password,
+        self.user = user
+        self.password = password
         self.session_kind = session_kind
-        print(op_kwargs)
+        self.poll_interval = poll_interval
         self.livy_url = https_con
         self.payload = json.dumps(op_kwargs)
 
     def execute(self, context):
         print("Spark Livy Operator initiated.....")
-        print("payload created is ....")
-        print(self.payload)
         headers = {
             "content-type": "application/json",
             "cache-control": "no-cache"
         }
         try:
-            response = requests.request("POST", url= self.https_con, auth=(self.user, self.password), data=self.payload, headers=headers)
-            print(response.text)
+            response = requests.request("POST", url= self.livy_url, auth=(self.user, self.password), data=self.payload, headers=headers)
             response_params = json.loads(response.text)
-            print(response_params)
+            batch_id = response_params['id']
+            print("Batch submitted to spark livy server: {}".format(batch_id))
+            ret_code = self.__poll_spark_job(batch_id)
+	    if ret_code == -1:
+	        raise AirflowException("Apache Spark Livy Job failed!")
         except:
             print("Unable to submit spark livy job.")
             raise
 
+    def __poll_spark_job(self, batch_id):
+        try:
+            state = "starting"
+            while state != "dead" and state != "success":
+                url = "{livy_url}{batch_id}".format(
+                    livy_url=self.livy_url, batch_id=batch_id)
+                headers = {
+                    'content-type': "application/json",
+                    'cache-control': "no-cache"
+                }
+                response = requests.request("GET", url, auth=(self.user, self.password), headers=headers)
+                response_params = json.loads(response.text)
+                state = response_params["state"]
+                print("Batch {id} status: {state}".format(id=batch_id, state=state))
+                if state != "success" and state != "dead":
+                    sleep(self.poll_interval)
+		else:
+		    print("Job finished!")
+
+            if state == "success":
+                print("Spark Livy Batch ID {} run succeed.".format(batch_id))
+                return 0
+            else:
+                print("Spark Livy Batch ID {} run failed.".format(batch_id))
+                return -1
+        except:
+            print("Unable to get spark batch status for batch_id {}".format(batch_id))
+            return -1
 
 class SparkLivyPlugin(AirflowPlugin):
     name = "spark_operator_plugin"
--- src/example_dag.py
+++ src/example_dag.py
@@ -5,8 +5,9 @@ from datetime import datetime, timedelta
 default_args = {
     'owner': 'Airflow',
     'depends_on_past': False,
-    'start_date': '2017-04-13',
-    'email': ['ddevang93@gmail.com'],
+    'start_date': datetime(2018,04,15),
+    'end_date': datetime(2018,04,18),
+    'email': ['depatel@starbucks.com'],
     'email_on_failure': False,
     'retries': 1,
     'retry_delay': timedelta(seconds=5)
@@ -15,11 +16,11 @@ default_args = {
 dag = DAG('sample_test', default_args=default_args, schedule_interval="0 11 * * *")
 
 t1 = SparkLivyOperator(
-    task_id='livy_op',
-    user='depatel',
-    password='patel',
-    https_conn='',
-    session_kind='',
-    poll_interval='',
-    op_kwargs={"file": "sample.jar"},
+    task_id="livy_op",
+    user="<user>",
+    password="<password>",
+    https_con="<cluster_livy>",
+    session_kind="spark",
+    poll_interval=60,
+    op_kwargs={"file": "sample_assembly.jar", "className": "Main" },
     dag=dag)
